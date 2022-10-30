b764124a17859bf7ed41565b690a66c8399fdec4
Jonghakseo <unqocn@gmail.com>
2022-10-30 16:57:55 +0900

    alias & 문서화 추가

--- hello_cargo/src/lib.rs
+++ hello_cargo/src/lib.rs
@@ -1,3 +1,13 @@
+/// Say hello fucntion
+///
+/// ```no_run
+/// fn hello() { println!("Hello, world!"); }
+/// ```
+///
+/// # Result
+/// ```shell
+/// Hello, world!
+/// ```
 pub fn hello() {
     println!("Hello, world!");
 }
